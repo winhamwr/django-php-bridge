aca37702b6751e8ed65b7675a6ba89c859dc06fa
Wes Winham <winhamwr@gmail.com>
2013-06-20 19:04:58 -0300

    Updated version to 0.1.0

--- django_php_bridge/__init__.py
+++ django_php_bridge/__init__.py
@@ -1,6 +1,6 @@
 """Authentication bridge between Django and PHP"""
 
-VERSION = (0, 0, 1, 'dev')
+VERSION = (0, 1, 0, '')
 
 __version__ = ".".join(map(str, VERSION[:-1]))
 __release__ = ".".join(map(str, VERSION))
